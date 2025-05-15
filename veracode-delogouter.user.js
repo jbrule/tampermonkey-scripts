731b1a039cb4b6734f061d1cf457599c09073612
jbrule <jbrulz@gmail.com>
2025-05-15 11:05:01 -0500

    Update veracode-delogouter.user.js

--- veracode-delogouter.user.js
+++ veracode-delogouter.user.js
@@ -4,19 +4,27 @@
 // @version      2025-05-14
 // @description  Click on the session continue button when it appears.
 // @author       Joshua Brule
-// @match        https://analysiscenter.veracode.com/*
-// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @match        https://*.analysiscenter.veracode.com/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    window.setInterval(function(){
-    let sessionContinueBtn = document.querySelector("#session-timeout-ok");
+    //If browser supports checkVisiblity() continue. Only run for top window
+    if("checkVisibility" in document.body && window.self == window.top){
+        console.log("TamperMonkey Veracode Delogouter is running on window ",window.location);
+        let timer = window.setInterval(function(){
 
-    if(sessionContinueBtn != null){
-        sessionContinueBtn.click();
+            let sessionContinueBtn = document.querySelector("#session-timeout-ok");
+
+            if(sessionContinueBtn && sessionContinueBtn.checkVisibility()){
+                sessionContinueBtn.click();
+                console.log("TamperMonkey Veracode Delogouter clicked on the Continue Button");
+            }
+        },30000);
     }
-    },30000);
+    else{
+        console.log("TamperMonkey Veracode Delogouter cannot run as checkVisibility() is not supported in this browser or window is not top");
+    }
 })();
